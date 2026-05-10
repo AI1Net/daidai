2e53cd71947d4b96d995a23fb876c3cd2e931a55
Aksara <mailepizo@gmail.com>
2026-05-10 21:53:50 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,12 @@ import "../styles/animations.css";
 export const metadata: Metadata = {
   title: "DAI DAI",
   description: "The People's Meme. The World's Currency.",
+
+  icons: {
+    icon: "/logos/ball.png",
+    shortcut: "/logos/ball.png",
+    apple: "/logos/ball.png",
+  },
 };
 
 export default function RootLayout({
